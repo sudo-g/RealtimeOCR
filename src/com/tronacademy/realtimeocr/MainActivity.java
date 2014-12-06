83616a641bc8061fa21d98eaff0d8601c9ec5116
George Xian <george_xian@live.com>
2014-12-06 15:34:14 +1300

    2_setup_tesseract: Moved allocation of inputFrame Mat as a class field so memory is instead explicitly allocated and freed on the "onCameraViewStarted()" and "onCameraViewStopped()" callbacks.
    Moved baseApi to be re-instantiated at every frame

--- src/com/tronacademy/realtimeocr/MainActivity.java
+++ src/com/tronacademy/realtimeocr/MainActivity.java
@@ -1,5 +1,11 @@
 package com.tronacademy.realtimeocr;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
@@ -7,11 +13,15 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.Mat;
+import org.opencv.android.Utils;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +31,10 @@ import android.view.SurfaceView;
 public class MainActivity extends ActionBarActivity implements CvCameraViewListener2 {
 	
 	private static final String TAG = "RealTimeOCR::MainActivity";
+	
+	// Fields of string paths for use with Tesseract OCR engine
+	public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/RealTimeOCR/";
+	public static final String lang = "eng";
 	
 	private JavaCameraView mOpenCvCameraView;
 	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -39,12 +53,48 @@ public class MainActivity extends ActionBarActivity implements CvCameraViewListe
             }
         }
     };
-    TessBaseAPI baseApi;	// interface to OCR engine
+    private Mat work;		// For use with storing camera input frames
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // create directory to store trained data
+        String[] paths = new String [] {DATA_PATH, DATA_PATH + "tessdata/"};
+        for (String path : paths) {
+        	File dir = new File(path);
+        	if (!dir.exists()) {
+        		if (!dir.mkdirs()) {
+        			Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+        		} else {
+        			Log.v(TAG, "Created directory " + path + "on sdcard");
+        		}
+        		
+        	}
+        }
+        
+        // move trained data into this directory
+        if (!(new File(DATA_PATH + "tessdata/"+ lang + "traineddata")).exists()) {
+        	try {
+        		AssetManager assetManager = getAssets();
+        		InputStream in = assetManager.open("eng.traineddata");
+        		OutputStream out = new FileOutputStream(DATA_PATH + "tessdata/eng.traineddata");
+        		
+        		// Transfer bytes from in to out
+        		byte[] buffer = new byte[1024];
+        		int len;
+        		while ((len = in.read(buffer)) > 0) {
+        			out.write(buffer, 0, len);
+        		}
+        		in.close();
+        		out.close();
+        		
+        		Log.v(TAG, "Copied " + lang + " traineddata");
+        	} catch (IOException e) {
+        		Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+        	}
+        }
         
         mOpenCvCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -86,20 +136,31 @@ public class MainActivity extends ActionBarActivity implements CvCameraViewListe
 
 	@Override
 	public void onCameraViewStarted(int width, int height) {
-		baseApi = new TessBaseAPI();	// instantiate OCR engine
+		work = new Mat();		// instantiate Mat here because it needs to be freed explicitly
 	}
 
 
 	@Override
 	public void onCameraViewStopped() {
-		baseApi.end();					// frees OCR engine instance
+		work.release();			// explicitly free input frame Mat
 	}
 
 
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-		// TODO Auto-generated method stub
-		Mat image = inputFrame.rgba();
+		// instantiate interface to OCR engine
+		TessBaseAPI baseApi = new TessBaseAPI();
+		baseApi.setDebug(true);
+		baseApi.init(DATA_PATH, lang);
+		
+		// Grab frame capture and convert to form usable by OCR engine
+		work = inputFrame.rgba();
+		Bitmap bmpImg = null;
+		Utils.matToBitmap(work, bmpImg);
+		
+		baseApi.setImage(bmpImg);
+		String recognizedText = baseApi.getUTF8Text();
+		baseApi.end();				// frees OCR engine instance
 		
 		
 		return inputFrame.rgba();
